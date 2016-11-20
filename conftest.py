0d6a5cf3884c4727ae65273fd0beca29777bb266
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2016-11-20 17:49:26 +0100

    Added conftest.py

---
+++ conftest.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Configuration of the pytest with CmdLine options, Fixtures and TearDown function
+"""
+import os
+
+import pytest
+
+from django.core.management import call_command
+from project.tests.utilities import create_test_data
+
+
+
+@pytest.fixture(scope='session')
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        create_test_data()
+
+
+@pytest.fixture(scope='session')
+def splinter_webdriver():
+    """Override splinter webdriver name."""
+    return 'chrome'
+
+@pytest.fixture(scope='session')
+def splinter_remote_url():
+    url = ''
+    try:
+        tunnel_id = os.environ['TRAVIS_JOB_NUMBER']
+        browser = os.environ['SAUCE_BROWSER']
+        platform = os.environ['SAUCE_PLATFORM']
+        desired_cap = {
+            'platform': platform,
+            'browserName': browser,
+            'tunnelIdentifier': tunnel_id
+        }
+
+        user = os.environ['SAUCE_USERNAME']
+        key = os.environ['SAUCE_ACCESS_KEY']
+        url = 'http://{0}:{1}@ondemand.saucelabs.com/wd/hub'.format(user, key)
+    except BaseException:
+        pass
+    return url
