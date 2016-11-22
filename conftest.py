53105432760340cc03adf6b09dd7b632217e511a
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2016-11-22 08:33:57 +0100

    No Screenshots

--- conftest.py
+++ conftest.py
@@ -20,17 +20,6 @@ def django_db_setup(django_db_setup, django_db_blocker):
 
 
 @pytest.fixture(scope='session')
-def splinter_remote_url():
-    url = ''
-    try:
-
-
-
-    except BaseException:
-        pass
-    return url
-
-@pytest.fixture(scope='session')
 def splinter_driver_kwargs():
     try:
         user = os.environ['SAUCE_USERNAME']
