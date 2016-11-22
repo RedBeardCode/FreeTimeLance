d204319ca501bbae83d6663729dca1ceff9a5616
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2016-11-22 08:50:42 +0100

    No Screenshots

--- conftest.py
+++ conftest.py
@@ -9,6 +9,8 @@ import os
 import pytest
 
 from django.core.management import call_command
+from splinter import Browser
+
 from project.tests.utilities import create_test_data
 
 
@@ -37,3 +39,20 @@ def splinter_driver_kwargs():
         return kwargs
     except BaseException:
         return {}
+
+
+@pytest.fixture(scope='session')
+def browser(request,browser_instance_getter):
+    user = os.environ['SAUCE_USERNAME']
+    key = os.environ['SAUCE_ACCESS_KEY']
+    url = 'http://{0}:{1}@ondemand.saucelabs.com/wd/hub'.format(user, key)
+    tunnel_id = os.environ['TRAVIS_JOB_NUMBER']
+    browser_name = os.environ['SAUCE_BROWSER']
+    platform = os.environ['SAUCE_PLATFORM']
+    browser = Browser(driver_name="remote",
+            url=url,
+            browser=browser_name,
+            platform=platform,
+                      tunnelIdentifier=tunnel_id,
+            name="Test of IE 9 on WINDOWS")
+    return browser
