c29d7b475b99faa9c937b512b1e67aeb8d35e29f
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2016-12-16 20:04:59 +0100

    Added SMTP Backend

--- FreeTimeLance/heroku_settings.py
+++ FreeTimeLance/heroku_settings.py
@@ -9,3 +9,11 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'freetimelance.herokuapp.com']
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'alfa3031.alfahosting-server.de'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ['ALFA_EMAIL_USER']
+EMAIL_HOST_PASSWORD = os.environ['ALFA_EMAIL_PWD']
